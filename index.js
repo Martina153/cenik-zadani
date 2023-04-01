2f83ca670befddbaff71c618c8f633823536eedd
Martina_Harsanikova <martinaharsanikova@gmail.com>
2023-04-01 16:39:17 +0200

    ok

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ for (let i = 1; i <= 100; i++) {
   }
 }*/
 
-const users = ['paja', 'kaja', 'vlasta', 'peta', 'alex', 'ja', 'misa']
+/*const users = ['paja', 'kaja', 'vlasta', 'peta', 'alex', 'ja', 'misa']
 
 for (let i = 0; i < users.length; i++) {
   if(users[i].length <= 4)
@@ -70,4 +70,142 @@ while (balance2>=0 && movement<amounts.length) {
   } else {
     console.log('Na účtu nebyl dosažen záporný zůstatek.');}
 
-//
+//Napište cyklus, který projde celé toto pole a všechny položky spojí 
+//dohromady do jednoho velkého řetězce jako níže. A výsledek vypište do konzole.
+
+const list = ['mouka', 'máslo', 'cukr', 'jablka', 'skořice']
+const result = list.join(',')
+console.log(result)
+//Upravte váš program tak, aby výsledný řetězec obsahoval HTML značky.
+//'<li>mouka</li><li>máslo</li><li>cukr</li><li>jablka</li><li>skořice</li>'
+
+const list2 = ['mouka', 'máslo', 'cukr', 'jablka', 'skořice'];
+let items = '';
+
+for (let item of list2) {
+  items += `<li>${item}</li>`;
+}
+
+console.log(items);
+
+//Napište cyklus WHILE, 
+//který do konzole postupně vypíše všechny časové údaje v rámci jednoho dne od 0:00 až po 23:59.
+let hour = 0; 
+while (hour < 24) {
+  let minute = 0;
+  while (minute < 60) {
+    console.log(`${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}`);
+    minute++;
+  }
+  hour++;
+}
+
+//Napište funkci roll, která simuluje hod kostkou.
+// Vrátí tedy náhodné číslo od 1 do 6 tak, že všechna čísla mají stejnou pravděpodobnost.
+
+
+function roll() {
+  return Math.floor(Math.random() * 6) + 1;
+}
+
+function rollUntilSix() {
+  let attempts = 0;
+  let result;
+  
+ while (result !== 6) {
+  result = roll();
+  attempts++;
+ }
+  
+  console.log(`Šestka padla na ${attempts}. pokus.`);
+}*/
+
+const numbers = [
+  -24, -11, 27, 29, -4, -28, -21, -14, 3, -8, 24, 19, -25, -2, -1, 11, 32, -31, 5
+]
+/*
+//Vypište na výstup všechna čísla.
+for (i in numbers) {
+console.log(numbers[i])}
+//Vypište na výstup druhé mocniny všech čísel.
+
+for (i in numbers) {
+  const square = Math.pow(numbers[i], 2);
+  console.log(square);
+}
+
+//Vypište na výstup pouze záporná čísla.
+
+for (i in numbers) {
+  if (numbers[i]<0) {
+    console.log(numbers[i])
+  }
+}
+
+//Vypište na výstup absolutní hodnotu všech čísel.
+for (i in numbers) {
+  const abs = Math.abs(numbers[i]);
+  console.log(abs);
+}
+
+//Vypište na výstup pouze sudá čísla.
+for (i in numbers) {
+  if (numbers[i]%2==0) {
+    console.log(numbers[i])
+  }
+}
+
+//Vypište na výstup pouze ta čísla, jejíchž absolutní hodnota je dělitelná třemi.
+
+for (i in numbers) {
+  if (numbers[i]%2!==0) {
+    console.log(numbers[i])
+  }
+}
+
+//Vypište na výstup jak daleko je každé číslo v seznamu od čísla 5.
+
+for (i in numbers) {
+  const distance = Math.abs(numbers[i] - 5);
+  console.log(`Vzdálenost čísla ${numbers[i]} od čísla 5 je ${distance}`);
+}
+
+//Vypište na výstup druhé mocnicny vzdáleností všech čísel od čísla 5.
+
+for (i in numbers) {
+  const distance = Math.pow(Math.abs(numbers[i] - 5),2)
+  console.log(distance)}
+
+//Spočítejte, kolik je v seznamu záporných čísel.
+let count =0
+for (i in numbers) {
+  if (numbers[i]<0) {
+    count++
+  }
+}
+console.log(count)*/
+
+//Spočítejte součet všech čísel v poli.
+/*let sum=0
+for (i in numbers) {
+  sum+=numbers[i]
+}
+console.log(sum)*/
+
+//Spočítejte průměr všech čísel v poli.
+let sum = 0
+
+for (let number of numbers) {
+  sum += number
+}
+const average = sum / numbers.length
+console.log(average)
+
+//Spočítejte součet všech kladných čísel v poli.
+let vysledok=0
+for (i in numbers) {
+  if(numbers[i]>0) {
+    vysledok+=numbers[i]
+  }
+}
+console.log(vysledok)
